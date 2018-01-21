eb7997e377a1f27f2f41ed4c0fa438f8f6f726c1
Francisco Mat <francisco@f5sites.com>
2018-01-21 01:10:33 -0200

    another nosleep bug try

--- pomodoro/pomodoro-functions.js
+++ pomodoro/pomodoro-functions.js
@@ -54,13 +54,24 @@ jQuery(document).ready(function ($) {
 	jQuery("#action_button_id").prop('disabled', false);
 
 	var noSleep = new NoSleep();
+
+	function enableNoSleep() {
+	  noSleep.enable();
+	  document.removeEventListener('click', enableNoSleep, false);
+	}
+
+	// Enable wake lock.
+	// (must be wrapped in a user input event handler e.g. a mouse or touch handler)
+	document.addEventListener('click', enableNoSleep, false);
+	
+	/*var noSleep = new NoSleep();
 	function enableNoSleep() {
 		noSleep.enable();
 		document.removeEventListener('touchstart', enableNoSleep, false);
 	}
 	// Enable wake lock.
 	// (must be wrapped in a user input event handler e.g. a mouse or touch handler)
-	document.addEventListener('touchstart', enableNoSleep, false);
+	document.addEventListener('touchstart', enableNoSleep, false);*/
 
 	
 	//EVERY 5s
