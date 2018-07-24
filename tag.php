725595d4aae5ee0ee1927115efcbe799a56923cc
Francisco Mat <francisco@f5sites.com>
2018-07-24 05:27:15 -0300

    Division by zero in

--- tag.php
+++ tag.php
@@ -170,7 +170,7 @@
 		$projetoInicio  = $query[count($query)-1]->post_date;
 		$projetoFinal = $query[0]->post_date;
 		$projetoDuracao = ceil((strtotime($projetoFinal) - strtotime($projetoInicio))/60/60/24);
-		
+		if($projetoDuracao==0)$projetoDuracao=1;
 		$projetoDiasTrabalhados = count($query_by_post_date);
 		/*foreach(array_keys($query_by_post_title) as $key){
 			echo $key."<hr />";
